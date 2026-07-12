5dd7ac5d139f26746bd9371e737e53b792cd6eca
sherrlyn18 <sherrlynanndrina.j2023cse@sece.ac.in>
2026-07-12 18:51:40 +0530

    production port

--- server.js
+++ server.js
@@ -40,10 +40,8 @@ app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
